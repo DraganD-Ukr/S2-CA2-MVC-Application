509d5dbfd6d8481778d9a8ced5ef7e6768dc25a5
Dmytro Drahan <dmitrijrolkin@gmail.com>
2025-03-13 14:48:55 +0000

    Initial project commit

---
+++  S2-CA2-MVC-MovieApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace S2_CA2_MVC_MovieApp.Data;
+
+public class ApplicationDbContext : IdentityDbContext {
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options) {
+    }
+}
---
+++  S2-CA2-MVC-MovieApp/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace S2_CA2_MVC_MovieApp.Models;
+
+public class ErrorViewModel {
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
