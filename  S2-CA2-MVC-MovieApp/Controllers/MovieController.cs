801e811db47baa1756be081314a6f35780a119a9
Dmytro Drahan <dmitrijrolkin@gmail.com>
2025-03-18 19:47:42 +0000

    feat: Add movie search functionality with results display

---
+++  S2-CA2-MVC-MovieApp/Controllers/MovieController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using S2_CA2_MVC_MovieApp.Data;
+using S2_CA2_MVC_MovieApp.Models;
+
+using S2_CA2_MVC_MovieApp.ViewModel;
+
+namespace S2_CA2_MVC_MovieApp.Controllers
+{
+    public class MovieController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+        private readonly ILogger<MovieController> _logger;
+
+        public MovieController(ILogger<MovieController> logger, ApplicationDbContext dbContext)
+        {
+            _logger = logger;
+            _dbContext = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Search(string searchTerm)
+        {
+            List<Movie> movies;
+
+            if (string.IsNullOrEmpty(searchTerm))
+            {
+                movies = _dbContext.Movies
+                    .Include(m => m.Genre)
+                    .OrderBy(m => m.Title)
+                    .Take(10)
+                    .ToList();
+
+                _logger.LogDebug("Returning first 10 movies from database.");
+            }
+            else
+            {
+                movies = _dbContext.Movies
+                    .Include(m => m.Genre) // Ensure Genre is included
+                    .Where(m => m.Title.ToLower().Contains(searchTerm.ToLower()) || 
+                                m.Description.ToLower().Contains(searchTerm.ToLower()))
+                    .OrderBy(m => m.Title)
+                    .ToList();
+
+                _logger.LogDebug($"Search term: {searchTerm}, Found movies: {string.Join(", ", movies.Select(m => m.Title))}");
+            }
+            
+            var searchViewModel = new SearchViewModel(movies, searchTerm);
+            
+            searchViewModel.Movies = movies;
+            
+            return View("Movie", searchViewModel);
+        }
+    }
+}
---
+++  S2-CA2-MVC-MovieApp/ViewModel/SearchViewModel.cs
@@ -0,0 +1,13 @@
+﻿using S2_CA2_MVC_MovieApp.Models;
+
+namespace S2_CA2_MVC_MovieApp.ViewModel;
+
+public class SearchViewModel {
+    public List<Movie> Movies { get; set; }
+    public string SearchTerm { get; set; }
+    
+    public SearchViewModel(List<Movie> movies, string searchTerm) {
+        Movies = movies;
+        SearchTerm = searchTerm;
+    }
+}
